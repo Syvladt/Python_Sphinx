d97adc2e827ef5a9a1362217992058073e2e30c7
Vladislav Sychugin <4myebay@mail.ru>
2024-01-31 19:28:48 +0500

    Добавьте файлы проекта.

---
+++ Sphinx.py
@@ -0,0 +1,30 @@
+'''
+Программа калькулятор. Самое первое приложение на Python
+Сначала надо авторизоваться
+'''
+if input('Введите имя пользователя: ') == 'admin':
+    if input('Введите пароль: ') == 'admin':
+        print('Вас приветствует калькулятор')
+        '''
+        Теперь можно посчитать, 2+2=22 ))))).
+        '''
+        operand_1 = float(input('Введите первый операнд: '))
+        action = input('Введите операцию (+, -, *, /): ')
+        operand_2 = float(input('Введите второй операнд: '))
+        if action == '+':
+            print (operand_1 + operand_2)
+        elif action == '-':
+            print (operand_1 - operand_2)
+        elif action == '*':
+            print (operand_1 * operand_2)
+        elif action == '/':
+            if operand_2 > 0:
+                print (operand_1 / operand_2)
+            else:
+                print ('Вы не окончили среднюю школу.')
+        else:
+            print ('Некорректная операция')
+    else:
+        print('Некорректный пароль.')
+else:
+    print('Некорректное имя пользователя.')
